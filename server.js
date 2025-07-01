e815c09fe3aa49df1f99c76877bd2aad89f1040a
rijans <makwanarijans@gmail.com>
2025-07-01 23:41:22 +0530

    project folder and initialization of socket-io

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("offer", (data) => {
+    socket.broadcast.emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    socket.broadcast.emit("answer", data);
+  });
+
+  socket.on("candidate", (data) => {
+    socket.broadcast.emit("candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
