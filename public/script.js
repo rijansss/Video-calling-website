59bc37b5d50b666f7296d41b489d73f83fee9fb1
rijans <makwanarijans@gmail.com>
2025-07-02 10:38:19 +0530

    setting up video call features

--- public/script.js
+++ public/script.js
@@ -1,1 +1,24 @@
-console.log("Frontend loaded!");
+const socket=io();
+const localVideo=document.getElementById('localVideo');
+const remoteVideo=document.getElementById('remoteVideo');
+
+let peerConnection;
+const config = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+};
+
+
+// getting access to webcam + mic 
+navigator.mediaDevices
+.getUserMedia({video:true,audio:true})
+.then((stream)=>{
+  localVideo.srcObject=stream;
+  window.localStream=stream ;    // it will save 
+})
+.catch((err)=>{
+  console.log("error accessing media devices ",err)
+})
